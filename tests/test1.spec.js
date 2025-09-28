eb6ecaba30ddf000ecf1c7afd19de8a83ae8a6e6
swapnil <spanbude17@gmail.com>
2025-09-28 20:27:34 +0530

    TC003

--- tests/test1.spec.js
+++ tests/test1.spec.js
@@ -22,3 +22,17 @@ await page.locator("//input[@id='monday']").click();
 
 
 })
+
+test('TC003',async({page})=>
+{
+
+await page.goto("https://testautomationpractice.blogspot.com/");
+await page.locator("//input[@id='name']").fill("Swapnil");
+await page.locator("//input[@id='email']").fill("swapnil17@gmail.com");
+await page.locator("//input[@id='female']").click();
+await page.locator("//input[@id='monday']").click();
+// const title=await page.title();
+await page.close();
+
+
+})
